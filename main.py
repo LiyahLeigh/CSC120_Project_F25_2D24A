6b37d4bfb3037893de8b4461d6d2bac6f57486ac
Taliyah Evans <tevans44@broncos.uncfsu.edu>
2025-09-19 08:09:05 -0400

    First commit: added main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+game_name = "Escaping"  # TODO: name your game project
+print("Welcome to " + game_name + "!")
+print("====================")  # TODO: add/remove "=" to line up with "!"
+print("Before we begin, what is your character's name?")
+
+name = input("> ")
+print("Great, " + name + "! Let's begin the adventure!")
