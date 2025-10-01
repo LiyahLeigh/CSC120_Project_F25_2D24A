7af77efa97be3de69a6133cd1b147d4581f46f22
Taliyah Evans <tevans44@broncos.uncfsu.edu>
2025-10-01 19:12:21 -0400

    Part 2: Added f-strings, player dictionary, random events, and stats update

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 game_name = "Escaping"  # TODO: name your game project
-print("Welcome to " + game_name + "!")
-print("====================")  # TODO: add/remove "=" to line up with "!"
-print("Before we begin, what is your character's name?")
+greeting = f"Welcome to {game_name}!"
+print(greeting)
+print(f'{"=" * len(greeting)}') # TODO: add/remove "=" to line up with "!"
 
-name = input("> ")
-print("Great, " + name + "! Let's begin the adventure!")
+name = input("Enter your character's name: ") #ask for the character's name
+
+player = {
+    'name': name,
+    'health': 100,
+    'coin': 0
+}
+
+import random
+events = ["find a coin", "meet a monster", "do nothing"]
+event = random.choice(events)
+print(f"While exploring, you {event}!")
+
+if event == "find a coin":
+    player['coin'] += 1
+    print(f"{player['name']} found a coin, {player['name']} now has {player['coin']} coins.")
+elif event == "meet a monster":
+    player['health'] -= 10
+    print(f"{player['name']} got hurt during the combat with monster, health is now {player['health']}.")
+else:
+    pass
+print(player)
